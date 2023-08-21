2c80d0d59da760ab82e1c7f84d3c612cc992759b
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-08-21 14:42:00 +0300

    update homework 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listEL = document.querySelector(".gallery");
+const galleryList = (array) => array.map(item => 
+    `<li class ="gallery__item"
+    <a class ="gallery__link" href ="${item.original}">
+    <img 
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"/>
+    </a>
+    </li> `)
+    .join("");
 
-console.log(galleryItems);
+
+const onImageClick = (e) => {
+    if (e.currentTarget === e.target) {
+        return;
+    }
+    const currentListItem = e.target;
+    const imageItem = currentListItem.dataset.source;
+   
+    const modalInstance = basicLightbox.create(`
+    <div class="modal">
+       <img 
+      class="gallery__image"
+      src="${imageItem}"
+      alt="${currentListItem.alt}" width="800" height="600"/>
+    </a>
+    </div>`)
+    modalInstance.show();
+
+    document.addEventListener("keydown", modalKeyClose);
+};
+    
+const modalKeyClose = (e) => {
+    if (e.key === "Escape") {
+        modalInstance.close();
+        
+    };
+};
+
+listEL.insertAdjacentHTML("beforeend", galleryList(galleryItems));
+listEL.addEventListener('click', onImageClick)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+console.log(galleryItems);
+const listEL = document.querySelector(".gallery");
+console.dir(listEL);
+const galleryList = (array) => array.map(item => 
+    `<li class ="gallery__item"
+    <a class ="gallery__link" href ="${item.original}">
+    <img 
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"/>
+    </a>
+    </li> `)
+    .join("");
+listEL.insertAdjacentHTML("beforeend", galleryList(galleryItems));
+
+
+const lightbox = $('.gallery a').SimpleLightbox( { captionsData: "alt", captionDelay: 250, });
